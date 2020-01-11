662b642e5475aae712526564fb50a52fd4d92549
Pablo Armas Martín <Pabloarmasm@gmail.com>
2020-01-11 15:07:44 +0000

    Modifiacion html home y añadiendo un dialog
    La pantalla home muestra ahora un mat grid con los elementos disponibles. Si se le clica en más información aparece un Dialogo con información acerca del producto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
+import { FruitDialog } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FruitDialog
   ],
   imports: [
     BrowserModule,
@@ -20,9 +24,14 @@ import { HomeComponent } from './home/home.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     HttpClientModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    FruitDialog
+  ]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { HttpService } from '../http.service';
-import {MatCardModule} from '@angular/material/card';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-home',
@@ -11,12 +12,44 @@ export class HomeComponent implements OnInit {
 
   collection : any;
 
-  constructor(private http : HttpService) { }
+  constructor(private http : HttpService, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.http.getData("fruit").subscribe(res =>{
       this.collection = res;
     });
+  }
+  algo(){
+    console.log("ola");
+  }
+  openPopUpWindow(idElement){
+    console.log(idElement);
+    const dialogRef = this.dialog.open(FruitDialog, {
+      width: '400px',
+      data: this.collection[idElement]
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('El dialogo se ha cerrado');
+    });
+  }
+
+}
+
+
+@Component({
+  selector: 'fruit-dialog',
+  templateUrl: 'fruit-dialog.html',
+  styleUrls: ['./home.component.css']
+})
+export class FruitDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<FruitDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: FruitDialog) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
